917f3cfc25429bc6e8c59e84fcc7ac31755de51a
Alejandro Dominguez <alejandroomin@gmail.com>
2021-10-06 14:25:46 +0200

    Script

---
+++ subir.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Se creara la pagina html"
+cd ~/Dropbox/2ASIR/Repositorios-GIT/pagina-html
+hugo -D
+cp ~/Dropbox/2ASIR/Repositorios-GIT/pagina-html/public/* ~/Dropbox/2ASIR/Repositorios-GIT/web-netlify/
+echo "Se actualizara la pagina estatica..."
+cd ~/Dropbox/2ASIR/Repositorios-GIT/web-netlify
+git add *
+git commit -am "Script"
+cd ~/Dropbox/2ASIR/Repositorios-GIT/pagina-html
+git add *
+git commit -am "Script"
+git push
