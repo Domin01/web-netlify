cedcbf8a71744eeb089305ff27902a8db9a27105
Alejandro Dominguez <alejandroomin@gmail.com>
2021-10-13 13:13:19 +0200

    He creado el fichero "hola.txt" en el directorio principal.

--- subir.sh
+++ subir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/Dropbox/2ASIR/Repositorios-GIT/pagina-html
-hugo -D >/dev/null
+hugo -D >/dev/null 2>&1
 cp -r ~/Dropbox/2ASIR/Repositorios-GIT/pagina-html/public/* ~/Dropbox/2ASIR/Repositorios-GIT/web-netlify/
 echo "Se actualizara la pagina estatica..."
 cd ~/Dropbox/2ASIR/Repositorios-GIT/web-netlify
@@ -9,8 +9,8 @@ git add *
 echo -n "Introduce el comentario para subir: "
 read respuesta
 git commit -am "$respuesta"
-git push >/dev/null
+git push >/dev/null 2>&1
 cd ~/Dropbox/2ASIR/Repositorios-GIT/pagina-html
 git add *
 git commit -am "$respuesta"
-git push >/dev/null
+git push >/dev/null 2>&1
