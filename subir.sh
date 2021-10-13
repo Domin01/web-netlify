96c0321fdf448e6563984af8753023f3b7239d2a
Alejandro Dominguez <alejandroomin@gmail.com>
2021-10-13 13:14:52 +0200

    Hola

--- subir.sh
+++ subir.sh
@@ -5,12 +5,13 @@ hugo -D >/dev/null 2>&1
 cp -r ~/Dropbox/2ASIR/Repositorios-GIT/pagina-html/public/* ~/Dropbox/2ASIR/Repositorios-GIT/web-netlify/
 echo "Se actualizara la pagina estatica..."
 cd ~/Dropbox/2ASIR/Repositorios-GIT/web-netlify
-git add *
-echo -n "Introduce el comentario para subir: "
+git add * >/dev/null 2>&1
+echo -n "Introduce el comentario al respecto: "
 read respuesta
-git commit -am "$respuesta"
+git commit -am "$respuesta" >/dev/null 2>&1
 git push >/dev/null 2>&1
 cd ~/Dropbox/2ASIR/Repositorios-GIT/pagina-html
-git add *
-git commit -am "$respuesta"
+git add * >/dev/null 2>&1
+git commit -am "$respuesta" >/dev/null 2>&1
 git push >/dev/null 2>&1
+echo "Se actualizaron los repositorios afectados."
