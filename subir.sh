5200d125cd3cf3d80e5a4a257067827796fff09e
Alejandro Dominguez <alejandroomin@gmail.com>
2021-10-13 13:09:55 +0200

    Hola

--- subir.sh
+++ subir.sh
@@ -2,14 +2,16 @@
 
 echo "Se creara la pagina html"
 cd ~/Dropbox/2ASIR/Repositorios-GIT/pagina-html
-hugo -D
+hugo -D >/dev/null 2>&1
 cp -r ~/Dropbox/2ASIR/Repositorios-GIT/pagina-html/public/* ~/Dropbox/2ASIR/Repositorios-GIT/web-netlify/
 echo "Se actualizara la pagina estatica..."
 cd ~/Dropbox/2ASIR/Repositorios-GIT/web-netlify
 git add *
-git commit -am "Script"
-git push
+echo -n "Introduce el comentario para subir: "
+read respuesta
+git commit -am "$respuesta"
+git push >/dev/null 2>&1
 cd ~/Dropbox/2ASIR/Repositorios-GIT/pagina-html
 git add *
-git commit -am "Script"
-git push
+git commit -am "$respuesta"
+git push >/dev/null 2>&1
